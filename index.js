f11c472106c95a44d95d4b5b25a59b968e23dc95
Mohsin <mohsin@localhost.localdomain>
2024-04-23 02:04:06 +0600

    post and get by using email

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clouster-1.fkyskeo.mongodb.net/?retryWrites=true&w=majority&appName=Clouster-1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const menuCollection = client.db("RestaurantDB").collection("menu");
+    const reviewCollection = client.db("RestaurantDB").collection("reviews");
+    const cartCollection = client.db("RestaurantDB").collection("carts");
+
+    app.get("/menu", async (req, res) => {
+      const menu = await menuCollection.find().toArray();
+      res.send(menu);
+    });
+    app.get("/reviews", async (req, res) => {
+      const menu = await reviewCollection.find().toArray();
+      res.send(menu);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const email = req?.query?.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Bistro World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
